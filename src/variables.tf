0bccba23ab1c15cf8350789d0b1c0d85fd6c8365
StepanST1 <the-bad1996@yandex.ru>
2026-07-03 23:12:47 +0300

    READY DZ

--- src/variables.tf
+++ src/variables.tf
@@ -1,6 +1,5 @@
 ###cloud vars
 
-
 variable "cloud_id" {
   type        = string
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
@@ -16,6 +15,7 @@ variable "default_zone" {
   default     = "ru-central1-a"
   description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
 }
+
 variable "default_cidr" {
   type        = list(string)
   default     = ["10.0.1.0/24"]
@@ -28,11 +28,79 @@ variable "vpc_name" {
   description = "VPC network & subnet name"
 }
 
-
 ###ssh vars
 
-variable "vms_ssh_root_key" {
+variable "vm_web_env" {
   type        = string
-  default     = "<your_ssh_ed25519_key>"
-  description = "ssh-keygen -t ed25519"
+  default     = "prod"
+  description = "Environment for web VM (prod/dev/stage)"
+}
+
+variable "vm_web_project" {
+  type        = string
+  default     = "platform"
+  description = "Project name for web VM"
+}
+
+variable "vm_web_role" {
+  type        = string
+  default     = "web"
+  description = "Role of web VM"
+}
+
+variable "vm_web_family" {
+  type        = string
+  default     = "ubuntu-2004-lts"
+  description = "yandex compute image family"
+}
+
+variable "vm_web_platform_id" {
+  type        = string
+  default     = "standard-v1"
+  description = "yandex compute instance platform id"
+}
+
+variable "vms_resources" {
+  description = "Resources configuration for all VMs"
+  type = map(object({
+    cores           = number
+    memory          = number
+    core_fraction   = number
+    hdd_size        = number
+    hdd_type        = string
+    preemptible     = bool
+    nat             = bool
+    serial_port_enable = number
+  }))
+  default = {
+    web = {
+      cores           = 2
+      memory          = 1
+      core_fraction   = 5
+      hdd_size        = 5
+      hdd_type        = "network-hdd"
+      preemptible     = true
+      nat             = true
+      serial_port_enable = 1
+    }
+    db = {
+      cores           = 2
+      memory          = 2
+      core_fraction   = 20
+      hdd_size        = 5
+      hdd_type        = "network-hdd"
+      preemptible     = true
+      nat             = true
+      serial_port_enable = 1
+    }
+  }
+}
+
+variable "metadata" {
+  description = "Common metadata for all VMs"
+  type = map(string)
+  default = {
+    serial-port-enable = "1"
+    ssh-keys           = "ubuntu:<your_ssh_ed25519_key>"
+  }
 }
---
+++ src/vms_platform.tf
@@ -0,0 +1,79 @@
+### DB VM variables
+
+variable "vm_db_env" {
+  type        = string
+  default     = "prod"
+  description = "Environment for db VM (prod/dev/stage)"
+}
+
+variable "vm_db_project" {
+  type        = string
+  default     = "platform"
+  description = "Project name for db VM"
+}
+
+variable "vm_db_role" {
+  type        = string
+  default     = "db"
+  description = "Role of db VM"
+}
+
+# variable "vm_db_family" {
+#   type        = string
+#   default     = "ubuntu-2004-lts"
+#   description = "yandex compute image family"
+# }
+
+# variable "vm_db_name" {
+#   type        = string
+#   default     = "netology-develop-platform-db"
+#   description = "yandex compute instance name"
+# }
+
+variable "vm_db_zone" {
+  type        = string
+  default     = "ru-central1-b"
+  description = "yandex compute instance zone"
+}
+
+variable "vm_db_platform_id" {
+  type        = string
+  default     = "standard-v3"
+  description = "yandex compute instance platform id"
+}
+
+# variable "vm_db_cores" {
+#   type        = number
+#   default     = 2
+#   description = "yandex compute instance resources cores"
+# }
+
+# variable "vm_db_memory" {
+#   type        = number
+#   default     = 2
+#   description = "yandex compute instance resources memory"
+# }
+
+# variable "vm_db_core_fraction" {
+#   type        = number
+#   default     = 20
+#   description = "yandex compute instance resources core-fraction"
+# }
+
+# variable "vm_db_preemptible" {
+#   type        = bool
+#   default     = true
+#   description = "yandex compute instance scheduling policy preemptible"
+# }
+
+# variable "vm_db_nat" {
+#   type        = bool
+#   default     = true
+#   description = "yandex compute instance network interface nat"
+# }
+
+# variable "vm_db_serial_port_enable" {
+#   type        = number
+#   default     = 1
+#   description = "yandex compute instance metadata serial-port-enable"
+# }
